416fe181f2dbdaec7ab618f5d1f0fa755c7d3534
sholehbaktiabadi <sholehbaktiabadi@gmail.com>
2024-10-26 12:34:10 +0700

    adjust search box

--- components/main-search.tsx
+++ components/main-search.tsx
@@ -9,7 +9,7 @@ export const MainSearch = () => {
             <View className="h-80 bg-slate-200 rounded-xl py-5">
                 <View className="flex flex-row items-center mx-auto">
                     <View className="basis-5/6">
-                        <TextInput className="border mx-2 my-3 rounded-lg p-3 border-sky-300 placeholder:text-slate-300 focus:outline-sky-500 focus:border-sky-500 focus:shadow focus:shadow-sky-200 text-slate-500" placeholder='Cari klinik' />
+                        <TextInput className="border mx-2 my-3 rounded-full px-3 py-1 border-sky-300 placeholder:text-slate-300 focus:outline-sky-500 focus:border-sky-500 focus:shadow focus:shadow-sky-200 text-slate-500" placeholder='Cari klinik' />
                     </View>
                     <View >
                         <FontAwesomeIcon icon={faCircleUser} size={40} color="gray" />
