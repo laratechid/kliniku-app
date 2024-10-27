7a44d88c8bf2f68ed69ec75fbf01901ab72fbe5d
sholehbaktiabadi <sholehbaktiabadi@gmail.com>
2024-10-28 00:00:52 +0700

    fix sub menu label

--- components-micro/sub-menu.tsx
+++ components-micro/sub-menu.tsx
@@ -14,7 +14,7 @@ export const CardImages = ({ titles }: { titles: string[] }) => {
           className={`h-20 w-[${width / 2}] bg-gray-200 rounded-xl rounded-lg overflow-hidden`}
           source={{ uri: "https://dummyimage.com/600x400/4f4f4e/ffffff" }} resizeMode="cover"
         >
-          <View className="absolute top-1 right-1 bg-gray-300 rounded-full">
+          <View className="absolute top-1 p-1 px-4 right-1 bg-gray-300 rounded-full">
             <Text className="text-gray-600 text-xs">{item}</Text>
           </View>
         </ImageBackground>
