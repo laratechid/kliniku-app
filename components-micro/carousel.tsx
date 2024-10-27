bdd5d9ea40635ec0fb58d5b32e6a2bca3989beab
sholehbaktiabadi <sholehbaktiabadi@gmail.com>
2024-10-27 11:40:56 +0700

    fix carousel responsive issue

--- components-micro/carousel.tsx
+++ components-micro/carousel.tsx
@@ -1,31 +1,25 @@
 import * as React from 'react';
-import { Dimensions, Image, View } from 'react-native';
+import { Dimensions, Image, View, StyleSheet } from 'react-native';
 import Carousel from 'react-native-reanimated-carousel';
 
 export const ImageCarousel = ({ images }: { images: string[] }) => {
-    // const width = Dimensions.get('window').width;
+    const { width } = Dimensions.get('window');
+
     return (
-        <View style={{ flex: 1 }}>
+        <View style={styles.container}>
             <Carousel
                 loop
-                width={340}
-                height={200}
+                width={width * 0.8} // 80% of screen width
+                height={(width) / 2} // Maintain aspect ratio (width / 2)
                 autoPlay={true}
                 data={images}
                 scrollAnimationDuration={1000}
                 onSnapToItem={(index) => console.log('current index:', index)}
                 renderItem={({ index, item }) => (
-                    <View
-                        key={index}
-                        style={{
-                            flex: 1,
-                            borderWidth: 1,
-                            justifyContent: 'center'
-                        }}
-                    >
+                    <View key={index} style={styles.imageContainer}>
                         <Image
                             source={{ uri: item }}
-                            style={{ width: 'auto', height: 100, borderRadius: 10 }}
+                            style={styles.image}
                             resizeMode="contain"
                         />
                     </View>
@@ -33,4 +27,21 @@ export const ImageCarousel = ({ images }: { images: string[] }) => {
             />
         </View>
     );
-}
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imageContainer: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        borderRadius: 10,
+    },
+});
